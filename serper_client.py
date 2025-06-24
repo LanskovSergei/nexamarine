9a42a5a16e74d0b1526b85b27367137b67bb7af4
LanskovSergei <backend.serge@gmail.com>
2025-06-24 11:22:20 +0700

    Update serper_client.py

--- serper_client.py
+++ serper_client.py
@@ -1,17 +1,18 @@
-# backend/app/core/serper_client.py
 import requests
+import os
 
-API_KEY = "a039b5e1bba82d68b0557cdc3169afca8d2a6bf9"
-API_URL = "https://google.serper.dev/search"
+SERPER_API_KEY = os.getenv("SERPER_API_KEY", "a039b5e1bba82d68b0557cdc3169afca8d2a6bf9")
+SERPER_URL = "https://google.serper.dev/search"
 
 def search_serper(query: str):
     headers = {
-        "X-API-KEY": API_KEY,
+        "X-API-KEY": SERPER_API_KEY,
         "Content-Type": "application/json"
     }
-    payload = {"q": query}
+    payload = {
+        "q": query
+    }
 
-    response = requests.post(API_URL, headers=headers, json=payload)
+    response = requests.post(SERPER_URL, headers=headers, json=payload)
     response.raise_for_status()
-
     return response.json()
